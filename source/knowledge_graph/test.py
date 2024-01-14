c8d58ae1f9f478e5f939d857e1e7e00207f322cc
spiderCodeMan <iglimanaj938@gmail.com>
2024-01-14 17:20:09 +0100

    Integrate neo4j python library and write first testing function

---
+++ source/knowledge_graph/test.py
@@ -0,0 +1,27 @@
+from neo4j import GraphDatabase
+
+class HelloWorldExample:
+
+    def __init__(self, uri):
+        self.driver = GraphDatabase.driver(uri)
+
+    def close(self):
+        self.driver.close()
+
+    def print_greeting(self, message):
+        with self.driver.session() as session:
+            greeting = session.execute_write(self._create_and_return_greeting, message)
+            print(greeting)
+
+    @staticmethod
+    def _create_and_return_greeting(tx, message):
+        result = tx.run("CREATE (a:Greeting) "
+                        "SET a.message = $message "
+                        "RETURN a.message + ', from node ' + id(a)", message=message)
+        return result.single()[0]
+
+
+if __name__ == "__main__":
+    greeter = HelloWorldExample("bolt://localhost:7687")
+    greeter.print_greeting("hello, world")
+    greeter.close()
